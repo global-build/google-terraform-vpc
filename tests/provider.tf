a80ba9b6be6052964cc0df892fb36cff136f08f2
trisha0996 <trisha0996@gmail.com>
2020-09-24 09:06:05 +0100

    Remove region

--- tests/provider.tf
+++ tests/provider.tf
@@ -1,13 +1,11 @@
 
 provider "google" {
   project = var.project_id
-  region  = var.region
   version = "3.11"
 }
 
 provider "google-beta" {
   project = var.project_id
-  region  = var.region
   version = "3.11"
 }
 
